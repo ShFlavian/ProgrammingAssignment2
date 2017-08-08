2a3afe0984c80792d0dc070d2560f343ce428f11
Sh Flavian <shflavian@hotmail.com>
2017-08-08 11:33:00 +1000

    Adding list() and comments to makeCacheMatrix()
    adding list contains  makeCacheMatrix()   functions.
    adding comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,13 @@
 
+# makeCacheMatrix Function creates an R object that stores a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         
         inverse_Obj<-NULL
 
+        
+        # set_Matrix function assigns value to x &  inverse_Obj objects in the makeCacheMatrix() environment
+        # And clears any value of inverse_Obj that had been cached by a prior execution of cacheSolve()
         
         set_Matrix<- function(y){
                 x<<-y
@@ -11,13 +15,19 @@ makeCacheMatrix <- function(x = matrix()) {
         }
         
         
+        # R retrieves inverse_Obj from the makeCacheMatrix()environment
         get_Inverse<- function() inverse_Obj
         
         
+        # R retrieves x from the makeCacheMatrix()environment
         get_Matrix<- function() x
         
         
+        # This assign the inverse_arg argument to inverse_Obj object in the makeCacheMatrix environment.
         set_Inverse<- function(inverse_arg) inverse_Obj <<-inverse_arg 
+        
+        # The makeCacheMatrix return object of type makeCacheMatrix()        
+        list(set_Matrix=set_Matrix, get_Inverse=get_Inverse, get_Matrix=get_Matrix,set_Inverse)
 }
 
 
