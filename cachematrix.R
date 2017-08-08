6e35460ce2ded931ec55a57b19d093d076f3edd8
Sh Flavian <shflavian@hotmail.com>
2017-08-08 11:25:26 +1000

    Modifying the  getter and setter functions in makeCacheMatrix ()
    adding function input arguments  and function body to
    set_Matrix
    get_Inverse
    get_Matrix
    set_Inverse

--- cachematrix.R
+++ cachematrix.R
@@ -4,15 +4,29 @@ makeCacheMatrix <- function(x = matrix()) {
         
         inverse_Obj<-NULL
 
-        set_Matrix<- function() {}
-        get_Inverse<- function(){}
-        get_Matrix<- function() {}
-        set_Inverse<- function(){}
+        
+        set_Matrix<- function(y){
+                x<<-y
+                inverse_Obj<<-NULL
+        }
+        
+        
+        get_Inverse<- function() inverse_Obj
+        
+        
+        get_Matrix<- function() x
+        
+        
+        set_Inverse<- function(inverse_arg) inverse_Obj <<-inverse_arg 
 }
 
 
-## Write a short comment describing this function
+
 
 cacheSolve <- function(x, ...) {
+        
+        
+        
+        
         ## Return a matrix that is the inverse of 'x'
 }
